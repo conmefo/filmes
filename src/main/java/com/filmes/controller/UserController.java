03afb539b09f853c734eac1ef2bcf93ccda63337
conmefo <hoquangminh757@gmail.com>
2025-11-17 21:24:47 +0700

    edit usercontroller and userservice

--- src/main/java/com/filmes/controller/UserController.java
+++ src/main/java/com/filmes/controller/UserController.java
@@ -1,14 +1,27 @@
 package com.filmes.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.filmes.dto.request.UserCreationRequest;
+import com.filmes.dto.request.UserUpdateRequest;
+import com.filmes.dto.response.ApiResponse;
+import com.filmes.dto.response.UserResponse;
 import com.filmes.model.User;
 import com.filmes.repository.DataRepository;
+import com.filmes.service.UserService;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -17,15 +30,46 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/api")
 public class UserController {
     @Autowired
-    DataRepository dataRepository;
+    private UserService userService;
 
-    @PostMapping("/register")
-    public User createUser(@RequestBody User user) {
-        return dataRepository.saveUser(user);
+    @PostMapping
+    ApiResponse<UserResponse> createUser(@RequestBody @Valid UserCreationRequest request) {
+        return ApiResponse.<UserResponse>builder()
+                .result(userService.createUser(request))
+                .build();
     }
 
-    @GetMapping("/{}")
-    public User (@RequestParam String param) {
-        return new String();
+    @GetMapping("")
+    ApiResponse<List<UserResponse>> getAllUsers() {
+        return ApiResponse.<List<UserResponse>>builder()
+                .result(userService.getAllUsers())
+                .build();
+    }
+
+    @GetMapping("/myInfo")
+    ApiResponse<UserResponse> getMyInfo(@PathVariable String userId) {
+        return ApiResponse.<UserResponse>builder()
+                .result(userService.getMyInfo(userId))
+                .build();
+    }
+
+    @PutMapping("/{userId}")
+    ApiResponse<UserResponse> updateUser(
+            @PathVariable String userId,
+            @RequestBody UserUpdateRequest request) {
+
+        return ApiResponse.<UserResponse>builder()
+                .result(userService.updateUser(userId, request))
+                .build();
+    }
+
+    @DeleteMapping("/{userId}")
+    ApiResponse<String> deleteUser(@PathVariable String userId) {
+
+        userService.deleteUser(userId);
+
+        return ApiResponse.<String>builder()
+                .result("User deleted successfully")
+                .build();
     }
 }
--- src/main/java/com/filmes/service/UserService.java
+++ src/main/java/com/filmes/service/UserService.java
@@ -1,17 +1,39 @@
 package com.filmes.service;
 
+import java.util.HashSet;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.stereotype.Service;
 
+import com.filmes.dto.request.UserCreationRequest;
+import com.filmes.dto.response.UserResponse;
+import com.filmes.exception.AppException;
+import com.filmes.exception.ErrorCode;
+import com.filmes.model.User;
 import com.filmes.repository.DataRepository;
+
+import jakarta.validation.Valid;
 
 @Service
 public class UserService {
     @Autowired
     DataRepository dataRepository;
 
-    public User createUser(User user){
-        
+    public UserResponse createUser(UserCreationRequest request) {
+        if (dataRepository.findUserByUsername(request.getUsername()) != null) {
+            throw new AppException(ErrorCode.USER_EXISTS);
+        }
+
+        User user = userMapper.toUser(request);
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        HashSet<String> roles = new HashSet<>();
+        roles.add(Role.USER.name());
+
+        user.setRoles(roles);
+
+        return userMapper.toUserResponse(userRepository.save(user));
     }
 }
